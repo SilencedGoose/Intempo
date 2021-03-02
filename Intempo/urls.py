9a2eaa1cefaa7a3a6652401167e962019a42591d
AnimatorOfSouls <fantailfishy@gmail.com>
2021-03-02 21:24:21 +0000

    added placeholder views added urls

--- Intempo/urls.py
+++ Intempo/urls.py
@@ -5,4 +5,9 @@ app_name = "intempo"
 
 urlpatterns = [
     path("home/", views.index, name="home"),
+    path("albums/", views.albums, name="albums"),
+    path("albums/album_name/", views.album_page, name="album_page"),
+    path("user/user_name/", views.profile, name="profile"),
+    path("sign-up/", views.signup, name="signup"),
+    path("login/", views.login, name="login"),
 ]
--- Intempo/views.py
+++ Intempo/views.py
@@ -10,7 +10,45 @@ def index(request):
     context_dict = {}
     context_dict["temp_message"] = "placeholder!"
 
-    #visitor_cookie_handler(request)
-
     response = render(request, 'intempo/index.html', context=context_dict)
+    return response
+
+
+def albums(request):
+    context_dict = {}
+    context_dict[""] = ""
+
+    response = render(request, 'intempo/albums.html', context=context_dict)
+    return response
+
+
+def album_page(request):
+    context_dict = {}
+    context_dict["album_name"] = "placeholder album name"
+
+    response = render(request, 'intempo/album_page.html', context=context_dict)
+    return response
+
+
+def profile(request):
+    context_dict = {}
+    context_dict["username"] = "placeholder username"
+
+    response = render(request, 'intempo/profile.html', context=context_dict)
+    return response
+
+
+def signup(request):
+    context_dict = {}
+    context_dict[""] = ""
+
+    response = render(request, 'intempo/signup.html', context=context_dict)
+    return response
+
+
+def login(request):
+    context_dict = {}
+    context_dict[""] = ""
+
+    response = render(request, 'intempo/login.html', context=context_dict)
     return response
